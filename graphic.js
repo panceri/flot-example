cd20678e5ae95826c45c696acb8894976a9fc144
Filipe Sguarizi Panceri <filipe.info@gmail.com>
2016-06-06 21:11:40 -0300

    First commit

---
+++ graphic.js
@@ -0,0 +1,20 @@
+$(function()){
+  var data = [
+	{ label: "Series1",  data: 10},
+	{ label: "Series2",  data: 30},
+	{ label: "Series3",  data: 90},
+	{ label: "Series4",  data: 70},
+	{ label: "Series5",  data: 80},
+	{ label: "Series6",  data: 110}
+  ];
+
+  var graphics = $("#graphics");
+  $.plot(placeholder, data, {
+    series: {
+	  pie: { 
+	    show: true
+	  }
+	}
+  });
+
+};
